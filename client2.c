4cd6fbb17e852a8bfe76e707334477961661f895
adamroke <Adam@Adams-MacBook-Air.local>
2016-05-31 18:39:55 -0700

    client2 having problems
    try using telnet

--- client2.c
+++ client2.c
@@ -1,31 +1,60 @@
-#include "csapp.h" 
+#include "csapp.h"
+#include <string.h>
 
 /* usage: ./echoclient host port */
 int portChecker(char* port);
 int main(int argc, char **argv)
-{ 
+{
 	//"server" stuff referrs to connecting to webserver, "browser" variables refer to connecting to user's browser, anything else is either local proxy data or i've forgotten to change the name
-	int serverfd, port, browserfd, browserreadfd, clientlen; 
+	int serverfd, browserfd, browserreadfd, clientlen;
 	struct sockaddr_in clientaddr;
-	char *host, serverBuffer[MAXLINE], header[MAXLINE];	//header not necessary if commented out if/else isn't needed, delete if that's the case 
+	char *host, serverBuffer[MAXLINE], hostBuffer[MAXLINE], header[MAXLINE];	//header not necessary if commented out if/else isn't needed, delete if that's the case
 	rio_t serverData;
-	rio_t browserData; 
-	host = argv[1];  port = atoi(argv[2]); 
+	rio_t browserData;
+	//rio_t browserData_init;
+
 	while(1){
-		serverfd = Open_clientfd(host, argv[2]);
+		//serverfd = Open_clientfd(argv[1], argv[2]);
 		browserfd = Open_listenfd(argv[3]);
 		browserreadfd = Accept(browserfd, (SA *)&clientaddr, &clientlen);	//then just do stuff with clientaddr for logging, see main.c for info on that
-		Rio_readinitb(&serverData, serverfd);
+		//Rio_readinitb(&serverData, serverfd);
 		Rio_readinitb(&browserData, browserreadfd);
+		//Rio_readinitb(&browserData_init, browserreadfd);
+
+
+		//set up connection to host
+		Rio_readlineb(&browserData, serverBuffer, MAXLINE);
+		Fputs(serverBuffer, stdout);
+		Rio_readlineb(&browserData, hostBuffer, MAXLINE);
+		Fputs(hostBuffer, stdout);
+		host = hostBuffer+6;
+		printf("ATTEMPT CONN: %s\n", host);
+		serverfd = Open_clientfd(host, argv[2]);
+		Rio_readinitb(&serverData, serverfd);
+		Rio_writen(serverfd, serverBuffer, strlen(serverBuffer));
+		Rio_writen(serverfd, hostBuffer, strlen(hostBuffer));
+
+
+		printf("CONNECTION ESTABLISHED TO : %s\n", host);
+
+		//now send the remainder of requests to the server
 		printf(":"); fflush(stdout);
+
+		int i = 0;
 		int checkStatus = 1;
 		while (checkStatus!=0) {
+			if(strcmp(serverBuffer, "\r\n") == 0) break;
 			Rio_readlineb(&browserData, serverBuffer, MAXLINE);	//GET request from browser
 			Fputs(serverBuffer, stdout);
 			Rio_writen(serverfd, serverBuffer, strlen(serverBuffer));  //sends data to server, get rid of placeholder code above this
-			if(strcmp(serverBuffer, "\r\n") == 0) break;	//when end of header detected, break and send data to server
-			//if(strcmp(serverBuffer, "\r\n") == 0) break;
+			if(strcmp(serverBuffer, "\r\n") == 0) break;
 		}
+
+		//Thought that this had to go here. delete
+		//serverfd = Open_clientfd(host, argv[2]);
+		//Rio_readinitb(&serverData, serverfd);
+		//Rio_writen(serverfd, serverBuffer, strlen(serverBuffer));  //sends data to server
+
 		//check first line for 200 OK and HTTP version
 		//ok not sure this is neccessary, so commented it out
 		//Rio_readlineb(&serverData, header,MAXLINE);
@@ -47,14 +76,14 @@ int main(int argc, char **argv)
 					strcpy(temp, strtok(NULL, " \n\t\r"));
 					bufferSize = atoi(temp);
 				}
-				
+
 			}
-			 
+
 			//Close(serverfd);
 			//return;        //ideally, just print the whole header in here instead of returning, but this'll work for now
 	    }*/
 		//else{
-			//Fputs(header,stdout);      
+			//Fputs(header,stdout);
 		Fputs(serverBuffer,stdout);	//TODO: ALL fputs should be changed to write back to client instead of to stdout (but for testing purposes this works)
 		while(checkStatus != 0){
 			Rio_readlineb(&serverData, serverBuffer, MAXLINE);	//RESPONSE message
@@ -89,10 +118,11 @@ int main(int argc, char **argv)
 		if(strlen(serverBuffer) > 0){	//ok, so this DOESNT work, but basically
 			printf("serverbuffer now |%s|\n", serverBuffer);	//browser sockets are getting closed too quickly, requests for images are getting lost
 		}*/
-		Close(serverfd); 
+		Close(serverfd);
 		Close(browserfd);
 		Close(browserreadfd);
 	}
+<<<<<<< Updated upstream
     	exit(0); 
 } 
 
@@ -121,3 +151,7 @@ int portChecker(char* port){     //TODO: actually implement this
 }
 
 
+=======
+    	exit(0);
+}
+>>>>>>> Stashed changes
